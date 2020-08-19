1832fd5a075db2b58355efa506a0125a3cd703ba
Tajutheen <tajutheen.info@gmail.com>
2020-08-19 15:33:48 +0530

    MyJavaProject

---
+++ MyJavaProject/src/com/cg/sample/TestCalculator.java
@@ -0,0 +1,14 @@
+package com.cg.sample;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		add (10,20);
+
+	}
+
+	private static void add(int i, int j) {
+		System.out.println("Result of add is"+ (i+j));		
+	}
+
+}
